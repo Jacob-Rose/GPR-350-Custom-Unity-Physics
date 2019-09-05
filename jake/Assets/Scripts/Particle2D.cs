41afe0e2b5f53771af59af469c6e95c0dff1ba18
student <student@mtl.local>
2019-09-05 17:15:53 -0400

    lab1 finished

--- jake/Assets/Scripts/Particle2D.cs
+++ jake/Assets/Scripts/Particle2D.cs
@@ -11,12 +11,26 @@ public class Particle2D : MonoBehaviour
     public Vector2 velocity;
     public Vector2 acceleration;
     public PhysicsType calculationType;
-
+    public PathType pathType;
+    public int physicsIterations = 2;
+    private float startTime; //used to randomize(not all objects in sync)
+    public enum PathType
+    {
+        Circle,
+        LineSin,
+        None
+    }
     public enum PhysicsType
     {
         Kinematic,
         Euler
     }
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
     // Start is called before the first frame update
 
     void updatePositionEulerExplicit(float deltaTime)
@@ -42,32 +56,37 @@ public class Particle2D : MonoBehaviour
         rotation = rotation + (angularVelocity * deltaTime) + (0.5f * angularAcceleration * deltaTime * deltaTime);
         angularVelocity += angularAcceleration * deltaTime;
     }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void FixedUpdate()
     {
-        if (calculationType == PhysicsType.Kinematic)
+        //calculation type
+        float fractionTime = Time.fixedDeltaTime / physicsIterations;
+        for (int i = 0; i < physicsIterations; i++)
         {
-            updatePositionKinematic(Time.fixedDeltaTime);
-            updateRotationKinematic(Time.fixedDeltaTime);
+            if (calculationType == PhysicsType.Kinematic)
+            {
+                updatePositionKinematic(fractionTime);
+                updateRotationKinematic(fractionTime);
+            }
+            else
+            {
+                updatePositionEulerExplicit(fractionTime);
+                updateRotationEulerExplicit(fractionTime);
+            }
         }
-        else
+        //acceleration stuff
+        if(pathType == PathType.Circle)
         {
-            updatePositionEulerExplicit(Time.fixedDeltaTime);
-            updateRotationEulerExplicit(Time.fixedDeltaTime);
+            //use velocity to ensure proper circle
+            acceleration.x = Mathf.Sin(Time.fixedTime - startTime);
+            acceleration.y = Mathf.Cos(Time.fixedTime - startTime);
         }
-        
+        else if(pathType == PathType.LineSin)
+        {
+            //same as circle but only on X
+            acceleration.x = Mathf.Sin(Time.fixedTime - startTime);
+        }
         transform.position = position;
         transform.eulerAngles = new Vector3(0, 0, rotation);
-        acceleration.x = Mathf.Sin(Time.fixedTime);
     }
 }
